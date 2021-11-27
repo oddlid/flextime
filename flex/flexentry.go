80629129bc83135caf3dd52a6c7c6d1d8e731e4d
Odd Eivind Ebbesen <odd.eivind.ebbesen@ericsson.com>
2021-11-27 01:01:29 +0100

    Added FlexEntry sorting and printing
    Change-Id: Id65835ff1abfb3cc65c3d32ab64f1e65219d8d52

--- flex/flexentry.go
+++ flex/flexentry.go
@@ -1,6 +1,25 @@
 package flex
 
-import "time"
+import (
+	"fmt"
+	"io"
+	"sort"
+	"strings"
+	"time"
+)
+
+type FlexEntrySortOrder uint8
+
+const (
+	shortDateFormat = "2006-01-02"
+)
+
+const (
+	FlexEntrySortByDateAscending = iota
+	FlexEntrySortByDateDescending
+	FlexEntrySortByAmountAscending
+	FlexEntrySortByAmountDescending
+)
 
 type FlexEntry struct {
 	Date   time.Time     `json:"date"`
@@ -8,6 +27,8 @@ type FlexEntry struct {
 }
 
 type FlexEntries []*FlexEntry
+type FlexEntriesByDate FlexEntries
+type FlexEntriesByAmount FlexEntries
 
 func (fes FlexEntries) getTotalFlex() time.Duration {
 	var total time.Duration
@@ -19,4 +40,53 @@ func (fes FlexEntries) getTotalFlex() time.Duration {
 
 func (fes FlexEntries) Len() int {
 	return len(fes)
+}
+
+func (fe FlexEntry) Print(w io.Writer) {
+	fmt.Fprintf(w, "%s : %v", fe.Date.Format(shortDateFormat), fe.Amount)
+}
+
+func (fes FlexEntries) Print(w io.Writer, indentString string, indentLevel int, sortOrder FlexEntrySortOrder) {
+	switch sortOrder {
+	case FlexEntrySortByDateAscending:
+		sort.Sort(FlexEntriesByDate(fes))
+	case FlexEntrySortByDateDescending:
+		sort.Sort(sort.Reverse(FlexEntriesByDate(fes)))
+	case FlexEntrySortByAmountAscending:
+		sort.Sort(FlexEntriesByAmount(fes))
+	case FlexEntrySortByAmountDescending:
+		sort.Sort(sort.Reverse(FlexEntriesByAmount(fes)))
+	}
+
+	prefix := strings.Repeat(indentString, indentLevel)
+
+	for _, fe := range fes {
+		fmt.Fprintf(w, "%s", prefix)
+		fe.Print(w)
+		fmt.Fprint(w, "\n")
+	}
+}
+
+func (febd FlexEntriesByDate) Len() int {
+	return len(febd)
+}
+
+func (febd FlexEntriesByDate) Swap(i, j int) {
+	febd[i], febd[j] = febd[j], febd[i]
+}
+
+func (febd FlexEntriesByDate) Less(i, j int) bool {
+	return febd[i].Date.Before(febd[j].Date)
+}
+
+func (feba FlexEntriesByAmount) Len() int {
+	return len(feba)
+}
+
+func (feba FlexEntriesByAmount) Swap(i, j int) {
+	feba[i], feba[j] = feba[j], feba[i]
+}
+
+func (feba FlexEntriesByAmount) Less(i, j int) bool {
+	return i < j
 }
